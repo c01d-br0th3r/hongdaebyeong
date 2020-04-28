1f05c2bff5131e28f06c2d906503f923ff21800d
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-04-28 16:11:46 +0900

    edit router

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Main from "../Routes/Main";
 import Header from "./Header";
 import QnA from "../Routes/QnA";
@@ -12,6 +17,7 @@ export default () => (
       <Route exact path="/" component={Main} />
       <Route exact path="/qna" component={QnA} />
       <Route exact path="/settings" component={Settings} />
+      <Redirect from="*" to="/" />
     </Switch>
   </Router>
 );
