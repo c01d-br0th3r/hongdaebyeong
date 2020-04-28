45a100ca56ef1c63b6f4fac0959f36cc6d17f835
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-04-28 16:26:52 +0900

    delete compose in redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./Store/Reducers";
 import createSagaMiddleware from "redux-saga";
@@ -9,13 +9,7 @@ import rootSaga from "./Store";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(sagaMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(rootSaga);
 
